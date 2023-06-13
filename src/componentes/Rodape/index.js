957d23add4fff7e28ee0b73efd421a49adb2b8ac
Anne Estevam <helivianne@gmail.com>
2023-06-13 10:16:08 -0300

    Rodapé finalizado

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -4,10 +4,9 @@ import './style.css';
 export default function Rodape() {
     return (
         <footer>
-                <div>
-                    <p>© 2023 Bruce Feathers Advocacia e Assessoria - Todos os direitos reservados</p>
-                </div>
-        
+            <div>
+                <p>© 2023 Bruce Feathers Advocacia e Assessoria - Todos os direitos reservados. <span>Desenvolvido por <a href="https://github.com/anneestevam" target="_blank">Anne Estevam</a>.</span></p>
+            </div>
         </footer>
     )
 }
